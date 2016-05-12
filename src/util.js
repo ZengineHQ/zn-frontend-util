418bfad907ebaba86b679c30eda4716462c4c374
Mike Carson <mikec@wizehive.com>
2016-05-12 14:24:33 -0400

    fix spacing

--- src/util.js
+++ src/util.js
@@ -14,16 +14,16 @@ plugin.service('wgnUtil', [
 		var Service = this;
 
         /**
-         * Get a UUID
-         *
-         * @return {String} UUID
-         */
-        this.getUUID = function(){
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            });
-        };
+		 * Get a UUID
+		 *
+		 * @return {String} UUID
+		 */
+		this.getUUID = function() {
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+				return v.toString(16);
+			});
+		};
 
 		/**
 		 * Shuffle an array (Fisher-Yates Shuffle)
@@ -51,7 +51,7 @@ plugin.service('wgnUtil', [
 			return array;
 		};
 
-    	return this;
+		return this;
 
 	}
 ]);
