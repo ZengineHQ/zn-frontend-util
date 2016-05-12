3cf35a7893f92b2f6cf36bb273485729646542cd
Mike Carson <mikec@wizehive.com>
2016-05-12 14:19:16 -0400

    add Fisher-Yates Shuffle to utils

--- src/util.js
+++ src/util.js
@@ -8,14 +8,14 @@ plugin.service('wgnUtil', [
 
         /**
          * Service
-         * 
+         *
          * @type {Object}
          */
 		var Service = this;
 
         /**
          * Get a UUID
-         * 
+         *
          * @return {String} UUID
          */
         this.getUUID = function(){
@@ -24,6 +24,32 @@ plugin.service('wgnUtil', [
                 return v.toString(16);
             });
         };
+
+		/**
+		 * Shuffle an array (Fisher-Yates Shuffle)
+		 *
+		 * @param  {array} array to shuffle
+		 *
+		 * @return {array} shuffled array
+		 */
+		this.shuffle = function(array) {
+			var currentIndex = array.length, temporaryValue, randomIndex;
+
+			// While there remain elements to shuffle...
+			while (0 !== currentIndex) {
+
+				// Pick a remaining element...
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+
+				// And swap it with the current element.
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
+
+			return array;
+		};
 
     	return this;
 
