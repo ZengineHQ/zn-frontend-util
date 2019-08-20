2062fa4c06024637af7aa4c41632f73cbd5599c4
Joe Bello <JoeBello82@gmail.com>
2019-08-20 16:56:03 -0400

    chore: formatting

--- src/util.js
+++ src/util.js
@@ -5,224 +5,223 @@
  */
 plugin.service('wgnUtil', [ function() {
 
-    var srv = this;
+	var srv = this;
 
-    /**
-     * Get a UUID
-     *
-     * @return {String} UUID
-     */
-    srv.getUUID = function() {
-     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-       function(c) { return parseInt(Math.random() * 16).toString(16); });
-    };
+	/**
+	 * Get a UUID
+	 *
+	 * @public
+	 * @returns {String} UUID
+	 */
+	srv.getUUID = function() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+		function(c) { return parseInt(Math.random() * 16).toString(16); });
+	};
 
-    /**
-     * Shuffle an array (Fisher-Yates Shuffle)
-     *
-     * @param  {array} array to shuffle
-     *
-     * @return {array} shuffled array
-     */
-    srv.shuffle = function(array) {
-      var currentIndex = array.length, temporaryValue, randomIndex;
+	/**
+	 * Shuffle an array (Fisher-Yates Shuffle)
+	 *
+	 * @public
+	 * @param  {array} array to shuffle
+	 * @returns {array} shuffled array
+	 */
+	srv.shuffle = function(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+		}
 
-      return array;
-    };
+		return array;
+	};
 
-    /**
-     * Parse query string into object
-     *
-     * @param  {String} query string
-     *
-     * @return {Object} params
-     */
-    srv.parseQueryString = function(keyValue) {
+	/**
+	 * Parse query string into object
+	 *
+	 * @public
+	 * @param  {String} query string
+	 * @returns {Object} params
+	 */
+	srv.parseQueryString = function(keyValue) {
 
-      var obj = {},
-        key_value,
-        key;
+		var obj = {},
+		key_value,
+		key;
 
-            var params = (keyValue || "").split('&');
+			var params = (keyValue || "").split('&');
 
-      params.forEach(function(keyValue) {
-        if (keyValue) {
-          key_value = keyValue.split('=');
-          key = decodeURIComponent(key_value[0]);
-          obj[key] = key_value[1] !== undefined ?
-            decodeURIComponent(key_value[1]) : true;
-        }
-      });
+		params.forEach(function(keyValue) {
+		if (keyValue) {
+			key_value = keyValue.split('=');
+			key = decodeURIComponent(key_value[0]);
+			obj[key] = key_value[1] !== undefined ?
+			decodeURIComponent(key_value[1]) : true;
+		}
+		});
 
-      return obj;
-    };
+		return obj;
+	};
 
-    /**
-      * Decode an encoded value.
-      *
-      * @private
-      * @param {string} value The value to be decoded.
-      *
-      * @return {string} The decoded value.
-      */
-    var _decode = function _decode(value) {
-      var escapes, keys, reg;
+	/**
+	 * Decode an encoded value.
+	 *
+	 * @private
+	 * @param {string} value The value to be decoded.
+	 * @returns {string} The decoded value.
+	 */
+	var _decode = function _decode(value) {
+		var escapes, keys, reg;
 
-      escapes = {
-        '\\*dollar\\*': '$',
-        '\\*forward-slash\\*': '/',
-        '\\*left-bracket\\*': '[',
-        '\\*right-bracket\\*': ']',
-        '\\*number\\*': '#',
-        '\\*period\\*': '.'
-      };
+		escapes = {
+		'\\*dollar\\*': '$',
+		'\\*forward-slash\\*': '/',
+		'\\*left-bracket\\*': '[',
+		'\\*right-bracket\\*': ']',
+		'\\*number\\*': '#',
+		'\\*period\\*': '.'
+		};
 
-      keys = Object.keys(escapes);
+		keys = Object.keys(escapes);
 
-      keys.forEach(function(exp) {
-        reg = new RegExp(exp, 'g');
-        value = value.replace ? value.replace(reg, escapes[exp]) : value;
-      });
+		keys.forEach(function(exp) {
+		reg = new RegExp(exp, 'g');
+		value = value.replace ? value.replace(reg, escapes[exp]) : value;
+		});
 
-      return value;
-    };
+		return value;
+	};
 
-    /**
-      * Encode a value.
-      *
-      * @private
-      * @param {string} value The value to be encoded.
-      *
-      * @return {string} The encoded value.
-      */
-    var _encode = function _encode(value) {
-      var escapes, keys, reg;
+	/**
+	 * Encode a value.
+	 *
+	 * @private
+	 * @param {string} value The value to be encoded.
+	 * @return {string} The encoded value.
+	 */
+	var _encode = function _encode(value) {
+		var escapes, keys, reg;
 
-      escapes = {
-        '\\$': '*dollar*',
-        '\\/': '*forward-slash*',
-        '\\[': '*left-bracket*',
-        '\\]': '*right-bracket*',
-        '\\#': '*number*',
-        '\\.': '*period*',
-      };
+		escapes = {
+		'\\$': '*dollar*',
+		'\\/': '*forward-slash*',
+		'\\[': '*left-bracket*',
+		'\\]': '*right-bracket*',
+		'\\#': '*number*',
+		'\\.': '*period*',
+		};
 
-      keys = Object.keys(escapes);
+		keys = Object.keys(escapes);
 
-      keys.forEach(function(exp) {
-        reg = new RegExp(exp, 'g');
-        value = value.replace ? value.replace(reg, escapes[exp]) : value;
-      });
+		keys.forEach(function(exp) {
+		reg = new RegExp(exp, 'g');
+		value = value.replace ? value.replace(reg, escapes[exp]) : value;
+		});
 
-      return value;
-    };
+		return value;
+	};
 
-    /**
-    * Format one, or multiple, values.
-    *
-    * @param {Array | Object | string} items The item(s) to be formatted.
-    * @param {boolean} parse A flag to indicate decoding or encoding;
-    * true to decode, false to encode.
-    * @param {Array} whitelist Items to be excluded from the formatting
-    * process
-    * @return {Object} The modified settings.
-    */
-    srv.format = function format(items, parse, whitelist) {
-      var formatter, keys, tmpKey;
+	/**
+	 * Format one, or multiple, values.
+	 *
+	 * @public
+	 * @param {Array|Object|string} items The item(s) to be formatted.
+	 * @param {boolean} parse A flag to indicate decoding or encoding;
+	 * true to decode, false to encode.
+	 * @param {Array} whitelist Items to be excluded from the formatting
+	 * process
+	 * @returns {Object} The modified settings.
+	 */
+	srv.format = function format(items, parse, whitelist) {
+		var formatter, keys, tmpKey;
 
-      formatter = parse ? _decode : _encode;
+		formatter = parse ? _decode : _encode;
 
-      whitelist = whitelist || [];
+		whitelist = whitelist || [];
 
-      if (Array.isArray(items)) {
+		if (Array.isArray(items)) {
 
-        items.forEach(function(item, index) {
-          items[index] = format(item, parse, whitelist);
-        });
+		items.forEach(function(item, index) {
+			items[index] = format(item, parse, whitelist);
+		});
 
-      } else if (typeof items === 'object') {
+		} else if (typeof items === 'object') {
 
-        keys = Object.keys(items);
+		keys = Object.keys(items);
 
-        keys.forEach(function(key) {
+		keys.forEach(function(key) {
 
-          if (items.hasOwnProperty(key)) {
+			if (items.hasOwnProperty(key)) {
 
-            tmpKey = format(key, parse, whitelist);
-            items[tmpKey] = format(items[key], parse, whitelist);
-            delete items[key];
+			tmpKey = format(key, parse, whitelist);
+			items[tmpKey] = format(items[key], parse, whitelist);
+			delete items[key];
 
-          }
+			}
 
-        });
+		});
 
-      } else if (whitelist.indexOf(items) === -1) {
+		} else if (whitelist.indexOf(items) === -1) {
 
-        items = formatter(items);
+		items = formatter(items);
 
-      }
+		}
 
-      return items;
-    };
+		return items;
+	};
 
-    /**
-     * Convert a string to camel case.
-     *
-     * @public
-     * @param {string} str The string to be converted to camel case
-     * @returns {*} The non-string value or the converted string
-     */
-    srv.camelCase = function camelCase(str) {
+	/**
+	 * Convert a string to camel case.
+	 *
+	 * @public
+	 * @param {string} str The string to be converted to camel case
+	 * @returns {*} The non-string value or the converted string
+	 */
+	srv.camelCase = function camelCase(str) {
 
-      if (typeof str !== 'string') { return str; }
+		if (typeof str !== 'string') { return str; }
 
-      return str
-        .replace(/(^[A-Z])|(\s[a-zA-Z])|(\s)/g,
-          function(value, capture1, capture2, capture3) {
+		return str
+		.replace(/(^[A-Z])|(\s[a-zA-Z])|(\s)/g,
+			function(value, capture1, capture2, capture3) {
 
-            if (capture1) { return capture1.toLowerCase(); }
+			if (capture1) { return capture1.toLowerCase(); }
 
-            if (capture2) { return capture2.trim(' ').toUpperCase(); }
+			if (capture2) { return capture2.trim(' ').toUpperCase(); }
 
-            if (capture3) { return capture3.trim(' '); }
+			if (capture3) { return capture3.trim(' '); }
 
-            return value;
+			return value;
 
-          }
-        );
+			}
+		);
 
-    };
+	};
 
-    /**
-     * Verify a trailing slash and add if needed.
-     *
-     * @public
-     * @param {string} url The string to verify and append a trailing slash
-     * @returns {*} The non-string value or the verified/modified string
-     */
-    srv.trailingSlash = function trailingSlash(url) {
+	/**
+	 * Verify a trailing slash and add if needed.
+	 *
+	 * @public
+	 * @param {string} url The string to verify and append a trailing slash
+	 * @returns {*} The non-string value or the verified/modified string
+	 */
+	srv.trailingSlash = function trailingSlash(url) {
 
-      if (typeof url !== 'string') { return url; }
+		if (typeof url !== 'string') { return url; }
 
-      return url.substring(url.length - 1, url.length) !== '/' ?
-        url.concat('/') : url;
+		return url.substring(url.length - 1, url.length) !== '/' ?
+		url.concat('/') : url;
 
-    };
+	};
 
-    return srv;
+	return srv;
 
-  }
-]);
+}]);
